dc9636a46be9b9b4b61ffd9fd63b3e7d8e0b263e
lilin <l1271823528@163,com>
2019-11-10 10:29:23 +0800

    上传项目

---
+++ src/main/java/com/baidu/call/config/Encryptor.java
@@ -0,0 +1,23 @@
+package com.baidu.call.config;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+
+public class Encryptor {
+
+    public static void main(String[] args) {
+        BasicTextEncryptor textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword("EbfYkitulv73I2p0mXI50JMXoaxZTKJ0");
+        String url = textEncryptor.encrypt("jdbc:mysql://localhost:3306/zycoo_coovox");
+        String name = textEncryptor.encrypt("root");
+        String password = textEncryptor.encrypt("123456");
+//解密内容
+//        String url = textEncryptor.decrypt("");
+//        String name = textEncryptor.decrypt("");
+//        String password = textEncryptor.decrypt("4EyN0xDLbnP2lsaayjl8fbIctj5bVIdD");
+
+
+        System.out.println(url);
+        System.out.println(name);
+        System.out.println(password);
+    }
+}
